30e8d2fbaed79712813eb38e67c9cf880591301b
drummyfish <tastyfish@seznam.cz>
2015-01-19 23:29:35 +0100

    terrain drawing

--- src/graphics.py
+++ src/graphics.py
@@ -8,12 +8,81 @@ import general
 import time
 import world
 import draw
+import random
 
-#-----------------------------------------------------------------------
+#=======================================================================
+
+  ## Holds images (i.e. main tile variations, corners etc.) of a game
+  # (terrain) tile, the corners are represented by a string in following
+  # format: cornar_AB_XY, where A is U or D (up, down), B is L or R
+  # (left, right) and X and Y are either 0 (opposite not present) or 1
+  # (opposite present).
+  # For example if there is a part of terrain as follows:
+  #   0 1
+  # 0 K L
+  # 1 K L
+  # then for example in the upper left corner of L at [1,1] there should
+  # be drawn a corner of K with identifier corner_UL_01.
+
+class TileImageContainer:
+  def init(self):
+    self.main_tile = [None,None,None,None] # main tile variations or alternatively animation frames
+    self.corner_UL_00 = None
+    self.corner_UL_01 = None
+    self.corner_UL_10 = None
+    self.corner_UL_11 = None
+
+    self.corner_UR_00 = None
+    self.corner_UR_01 = None
+    self.corner_UR_10 = None
+    self.corner_UR_11 = None
+
+    self.corner_DL_00 = None
+    self.corner_DL_01 = None
+    self.corner_DL_10 = None
+    self.corner_DL_11 = None
+
+    self.corner_DR_00 = None
+    self.corner_DR_01 = None
+    self.corner_DR_10 = None
+    self.corner_DR_11 = None
+
+  def __init__(self,filename):
+    self.init()
+    self.load_from_file(filename)
+
+  def load_from_file(self,filename):
+    image = pygame.image.load(filename)
+
+    self.main_tile[0] = image.subsurface(68,0,general.TILE_WIDTH,general.TILE_HEIGHT)
+    self.main_tile[1] = image.subsurface(102,0,general.TILE_WIDTH,general.TILE_HEIGHT)
+    self.main_tile[2] = image.subsurface(68,28,general.TILE_WIDTH,general.TILE_HEIGHT)
+    self.main_tile[3] = image.subsurface(102,28,general.TILE_WIDTH,general.TILE_HEIGHT)
+
+    self.corner_UL_00 = image.subsurface(51,42,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UL_01 = image.subsurface(17,42,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UL_10 = image.subsurface(51,14,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UL_11 = image.subsurface(17,14,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UR_00 = image.subsurface(0,42,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UR_01 = image.subsurface(34,42,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UR_10 = image.subsurface(0,14,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_UR_11 = image.subsurface(34,14,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DL_00 = image.subsurface(51,0,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DL_01 = image.subsurface(17,0,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DL_10 = image.subsurface(51,28,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DL_11 = image.subsurface(17,28,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DR_00 = image.subsurface(0,0,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DR_01 = image.subsurface(34,0,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DR_10 = image.subsurface(0,28,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+    self.corner_DR_11 = image.subsurface(34,28,general.SUBTILE_WIDTH,general.SUBTILE_HEIGHT)
+
+#=======================================================================
 
   ## Assembles images out of image resources.
 
 class ImageCompositor:
+
+  #---------------------------------------------------------------------
 
   ## Makes a character image with given body, head, animation type, frame
   #  and gear
@@ -75,31 +144,157 @@ class ImageCompositor:
 
   #---------------------------------------------------------------------
 
+  ## Helper private method that returns the priority of given tile type
+  #  or 0 if the argument is not of TileType class.
+
+  def __tile_priority(self, tile_type):
+    try:
+      return tile_type.get_priority()
+    except Exception:
+      return 0
+
+  #---------------------------------------------------------------------
+
+  ## Helper private method, returns ordered list of all tile priorities
+  #  that appear in given TerrainArray object.
+
+  def __make_tile_priority_list(self, terrain_array):
+    result = []
+
+    for j in range(terrain_array.get_height()):
+      for i in range(terrain_array.get_width()):
+        priority = self.__tile_priority(terrain_array.get_tile_type(i,j))
+
+        if not priority in result:
+          result.append(priority)
+
+    return sorted(result)
+
+  #---------------------------------------------------------------------
+
   ## Makes a terrain image.
   #
-  #  @param terrain_array list of lists of tuples(2D array or pairs) of
-  #         representing the terrain, where the pair at each position is
-  #         (A, B), where A is an integer value corresponding to tiles
-  #         in tile_key, i.e. 0 is first tile in tile_keys, 1 is
-  #         the second etc., if the value is out of range of tile_key
-  #         list (even negative), transparent tile is drawn, B is
-  #         the tile variant (0 to 3 including)
-  #  @param tile_key list of TileType objects
+  #  @param terrain_array TerrainArray object to be drawn
   #  @return Surface object - the generated image
 
-  def make_terrain_image(self, terrain_array, tile_key):
-    result_image = pygame.Surface((len(terrain_array[0]) * general.TILE_WIDTH,len(terrain_array) * general.TILE_HEIGHT))
-
+  def make_terrain_image(self, terrain_array):
+    result_image = pygame.Surface((terrain_array.get_width() * general.TILE_WIDTH, terrain_array.get_height() * general.TILE_HEIGHT))
     result_image.fill((255,255,255,0))
 
-    for j in range(len(terrain_array)):
-      for i in range(len(terrain_array[j])):
+    tile_pictures = {}
 
-        tile_number = terrain_array[j][i][0]
-        tile_variant = terrain_array[j][i][1]
+    for current_priority in self.__make_tile_priority_list(terrain_array):      # draw the terrain in layers
 
-        if tile_number in range(len(tile_key)):
-          result_image.blit(tile_key[tile_number].main_tile[tile_variant],(i * general.TILE_WIDTH,j * general.TILE_HEIGHT))
+      for j in range(terrain_array.get_height()):
+        for i in range(terrain_array.get_width()):
+          tile_type = terrain_array.get_tile_type(i,j)
+
+          if tile_type == None:
+            continue
+
+          tile_priority = tile_type.get_priority()
+
+          if not tile_priority == current_priority:   # only draw one priority at a time
+            continue
+
+          variant = terrain_array.get_tile_variant(i,j)
+          tile_id = tile_type.get_identifier()
+
+          if not (tile_id in tile_pictures):  # lazy image loading
+            tile_pictures[tile_id] = TileImageContainer(os.path.join(general.RESOURCE_PATH,tile_type.get_filename()))
+
+          # draw the main tile:
+          result_image.blit(tile_pictures[tile_id].main_tile[variant],(i * general.TILE_WIDTH,j * general.TILE_HEIGHT))
+
+          # draw corners and borders:
+
+          # UL corner:
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
+           result_image.blit(tile_pictures[tile_id].corner_DR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+
+          # UR corner
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
+            result_image.blit(tile_pictures[tile_id].corner_DL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+
+          # DR corner
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
+            result_image.blit(tile_pictures[tile_id].corner_UL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
+
+          # DL corner
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)) and \
+             tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
+            result_image.blit(tile_pictures[tile_id].corner_UR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
+
+          # upper border
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)):
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)):  # left
+              helper_image = tile_pictures[tile_id].corner_DL_01
+            else:
+              helper_image = tile_pictures[tile_id].corner_DL_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)):  # right
+              helper_image = tile_pictures[tile_id].corner_DR_01
+            else:
+              helper_image = tile_pictures[tile_id].corner_DR_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH + general.SUBTILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+
+          # left border
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)):  # up
+              helper_image = tile_pictures[tile_id].corner_UR_10
+            else:
+              helper_image = tile_pictures[tile_id].corner_UR_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT))
+
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)):  # down
+              helper_image = tile_pictures[tile_id].corner_DR_10
+            else:
+              helper_image = tile_pictures[tile_id].corner_DR_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.SUBTILE_HEIGHT))
+
+          # right border
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)):  # up
+              helper_image = tile_pictures[tile_id].corner_UL_10
+            else:
+              helper_image = tile_pictures[tile_id].corner_UL_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT))
+
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)):  # down
+              helper_image = tile_pictures[tile_id].corner_DL_10
+            else:
+              helper_image = tile_pictures[tile_id].corner_DL_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT + general.SUBTILE_HEIGHT))
+
+          # lower border
+          if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)):
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)):  # left
+              helper_image = tile_pictures[tile_id].corner_UL_01
+            else:
+              helper_image = tile_pictures[tile_id].corner_UL_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
+
+            if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)):  # right
+              helper_image = tile_pictures[tile_id].corner_UR_01
+            else:
+              helper_image = tile_pictures[tile_id].corner_UR_11
+
+            result_image.blit(helper_image,(i * general.TILE_WIDTH + general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
 
     pygame.draw.rect(result_image,(0,0,0,255),pygame.Rect(0,0,result_image.get_width(),result_image.get_height()),1)
 
@@ -123,18 +318,40 @@ imgs.append(myyy.make_character_image(general.RACE_HUMAN,general.GENDER_MALE,2,g
 imgs.append(myyy.make_character_image(general.RACE_HUMAN,general.GENDER_MALE,2,general.ANIMATION_IDLE_LEFT,1))
 imgs.append(myyy.make_character_image(general.RACE_HUMAN,general.GENDER_MALE,2,general.ANIMATION_IDLE_UP,1))
 
-tile1 = general.TileImageContainer("../resources/tile_grass.png")
-tile2 = general.TileImageContainer("../resources/tile_snow.png")
+tile1 = world.TileType(1,"tile_grass.png")
+tile2 = world.TileType(2,"tile_snow.png")
 
-ter_array = [[(1,0), (1,0), (1,2), (0,2), (0,0)],
-             [(1,2), (1,0), (1,1), (1,0), (1,0)],
-             [(1,1), (1,1), (1,1), (1,0), (1,0)],
-             [(1,1), (1,1), (1,2), (1,1), (1,1)],
-             [(1,2), (2,0), (0,0), (0,0), (0,0)],
-             [(0,1), (1,0), (0,2), (2,1), (0,1)],
-             [(0,2), (1,2), (0,0), (2,1), (2,0)]]
+print(tile1.get_identifier())
+print(tile2.get_identifier())
 
-ter = myyy.make_terrain_image(ter_array,[tile1,tile2])
+ter_array = world.TerrainArray(12,10)
+
+for j in range(10):
+  for i in range(12):
+    number = random.randint(0,2)
+
+    if number == 0:
+      ter_array.set_tile(i,j,tile1,0)
+    elif number == 1:
+      ter_array.set_tile(i,j,tile2,0)
+
+#ter_array.set_tile(2,1,tile1,0)
+#ter_array.set_tile(3,1,tile1,0)
+#ter_array.set_tile(1,2,tile1,0)
+#ter_array.set_tile(2,2,tile1,0)
+#ter_array.set_tile(3,2,tile1,0)
+#ter_array.set_tile(4,2,tile2,0)
+#ter_array.set_tile(2,3,tile1,0)
+#ter_array.set_tile(3,3,tile2,0)
+#ter_array.set_tile(4,3,tile2,0)
+#ter_array.set_tile(2,4,tile1,0)
+#ter_array.set_tile(4,4,tile2,0)
+#ter_array.set_tile(4,5,tile2,0)
+#ter_array.set_tile(1,6,tile2,0)
+#ter_array.set_tile(2,6,tile2,0)
+#ter_array.set_tile(4,6,tile2,0)
+
+ter = myyy.make_terrain_image(ter_array)
 
 i = 0
 
--- src/world.py
+++ src/world.py
@@ -6,13 +6,106 @@ import os
 import pygame
 import general
 import time
+import sys
 
-#-----------------------------------------------------------------------
+#=======================================================================
 
-  ## Represents a game file type. I.e. not a single tile but rather
+  ## Represents a game tile type. I.e. not a single tile but rather
   #  a type of tile, such as grass, road etc.
 
 class TileType:
-  priority = 0         #< tile priority, this affects how the tiles' borders overlap
-  image_filename = ""  #< name of the resource image file in which the tile is saved
-  is_animated = False
+  last_identifier = 0
+
+  def init(self):
+    self.priority = 0
+    self.filename = ""
+    self.identifier = TileType.last_identifier
+    TileType.last_identifier += 1
+
+  def __init__(self):
+    self.init()
+
+  ## Initialises a new object with given attribute values.
+  #
+  #  @param priority integer tile priority, this affects how the tiles
+  #         overlap
+  #  @param filename name of the image for the tile
+
+  def __init__(self, priority, filename):
+    self.init()
+    self.priority = priority
+    self.filename = filename
+
+  def get_priority(self):
+    return self.priority
+
+  def get_filename(self):
+    return self.filename
+
+  ## Gets the tile unique integer identifier.
+  #
+  #  @return unique integer identifier
+
+  def get_identifier(self):
+    return self.identifier
+
+#=======================================================================
+
+  ## Represents a part of terrain made of tiles. It is basically a 2D
+  #  array of pairs [TileType reference,variant (int)]
+
+class TerrainArray:
+  def __init__(self, width, height):
+    #initialize the terrain array, it's format is [x][y][tiletype(int),]:
+
+    self.terrain_array = [[[None,0] for i in range(height)] for j in range(width)]
+
+  def get_width(self):
+    return len(self.terrain_array)
+
+  def get_height(self):
+    return len(self.terrain_array[0])
+
+  ## Sets the tile at given position. If the position is outside the
+  #  terrain, nothing happens.
+
+  def set_tile(self, x, y, tile_type, variant):
+    try:
+      self.terrain_array[x][y][0] = tile_type
+      self.terrain_array[x][y][1] = variant
+    except IndexError:
+      return
+
+  ## Gets the tile type at given position.
+  #
+  #  @return tile type object at [x,y], if the position is outside
+  #          terrain array, closest tile type is returned
+
+  def get_tile_type(self, x, y):
+    return self.terrain_array[general.saturate(x,0,self.get_width() - 1)][general.saturate(y,0,self.get_height() - 1)][0]
+
+  ## Same as get_tile_type, just returns the tile variant.
+
+  def get_tile_variant(self, x, y):
+    return self.terrain_array[general.saturate(x,0,self.get_width() - 1)][general.saturate(y,0,self.get_height() - 1)][1]
+
+  ## Debug purpose method.
+
+  def print_out(self):
+    print("--------")
+
+    for j in range(ter_ar.get_height()):
+      for i in range(ter_ar.get_width()):
+        tile = self.get_tile_type(i,j)
+        variation = self.get_tile_variant(i,j)
+
+        if tile == None:
+          sys.stdout.write("N " + str(variation) + "    ")
+        else:
+          sys.stdout.write(tile.filename + " " + str(variation) + "    ")
+      print("")
+
+#=======================================================================
+
+
+
