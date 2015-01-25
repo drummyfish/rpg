7feca4b508f2b943a0316ee702e1debdf19c032e
drummyfish <tastyfish@seznam.cz>
2015-01-25 13:29:47 +0100

    resources

--- src/world.py
+++ src/world.py
@@ -7,6 +7,7 @@ import pygame
 import general
 import time
 import sys
+import numpy
 
 #=======================================================================
 
@@ -29,7 +30,9 @@ class TileType:
   #
   #  @param priority integer tile priority, this affects how the tiles
   #         overlap
-  #  @param filename name of the image for the tile
+  #  @param filename name of the image for the tile, only the image
+  #         name must be given without a path to it, for example:
+  #         "grass.png"
 
   def __init__(self, priority, filename):
     self.init()
@@ -107,5 +110,66 @@ class TerrainArray:
 
 #=======================================================================
 
+  ## Represents a concrete tile in game world, consists of tile type
+  #  plus objects at the tile.
 
+class TileInfo:
 
+  def __init__(self, tile_type, game_object_list):
+    self.tile_type = tile_type
+    self.objects = game_object_list
+
+#=======================================================================
+
+  ## Represents the game world as a 2D array of game tiles plus objects
+  #  in the world.
+
+class World:
+
+  ## Initialises a new empty world of given size.
+
+  def __init__(self, width, height):
+    self.world_array = numpy.empty((width,height),dtype = object)   # using numpy for the large world array, because the standard
+                                                                    # Python lists might be too large and not efficient
+    for j in range(len(self.world_array[0])):
+      for i in range(len(self.world_array)):
+        self.world_array[i,j] = TileInfo(None,None)
+
+  def set_tile_type(self, x, y, tile_type):
+    try:
+      self.world_array[x,y].tile_type = tile_type
+    except Exception:
+      return
+
+  ## Gets the tile type at given position
+  #
+  #  @param x x coordinate
+  #  @param y y coordinate
+  #  @return TileType object representing the tile type, or none if
+  #          there was no file at given position or the coordinates
+  #          were out of the world
+
+  def get_tile_type(self, x, y):
+    try:
+      return self.world_array[x,y].tile_type
+    except Exception:
+      return None
+
+  ## debug purposes method
+
+  def print_out(self):
+    print("--------")
+
+    for j in range(len(self.world_array[0])):
+      for i in range(len(self.world_array)):
+        if self.world_array[i,j].tile_type == None:
+          sys.stdout.write("N ")
+        else:
+          sys.stdout.write(str(self.world_array[i,j].tile_type.get_identifier()) + " ")
+      print("")
+
+t1 = TileType(1,"tile_grass.png")
+
+w = World(25,30)
+w.set_tile_type(2,1,t1)
+w.print_out()
