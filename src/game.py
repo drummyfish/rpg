4e694541f580f678c96c975511687305fca0633e
drummyfish <tastyfish@seznam.cz>
2015-01-28 14:31:10 +0100

    animated tiles work

--- src/game.py
+++ src/game.py
@@ -1,134 +1,22 @@
 ## \file general.py
 #  this file is the main game file
 
+
+import graphics
 import general
 import world
 import pygame
-import graphics
 import math
-
-## Efficiently renders a part of the game world with given settings.
-#
-#  The object of WorldRenderer class keeps a reference to World object
-#  which it will be rendering. It keeps a pre-rendered image of the
-#  world's active area terrain so it can be rendered quickly for
-#  real-time viewing. WorldRenderer will smartly change the world active
-#  area when the view shifts to the edge of the current area.
-
-class WorldRenderer:
-
-  ## view width in pixels
-  VIEW_WIDTH = 640
-
-  ## view height in pixels
-  VIEW_HEIGHT = 480
-
-  ## view width in pixels
-  VIEW_WIDTH_TILES = math.ceil(VIEW_WIDTH / general.TILE_WIDTH)
-
-  ## view height in pixels
-  VIEW_HEIGHT_TILES = math.ceil(VIEW_HEIGHT / general.TILE_HEIGHT)
-
-  ## along with VIEW_WIDTH and VIEW_HEIGHT determines the active area
-  #  size, which is width: VIEW_WIDTH_TILES + 2 * TILE_PADDING, height:
-  #  VIEW_HEIGHT_TILES + 2 * TILE_PADDING
-
-  TILE_PADDING = 10
-
-  ## active area width in tiles
-
-  ACTIVE_AREA_WIDTH = VIEW_WIDTH_TILES + 2 * TILE_PADDING
-
-  ## active area height in tiles
-
-  ACTIVE_AREA_HEIGHT = VIEW_HEIGHT_TILES + 2 * TILE_PADDING
-
-  def _view_top_left_tiles(self):
-    return (math.floor(self._view_top_left[0] / general.TILE_WIDTH),math.floor(self._view_top_left[1] / general.TILE_HEIGHT))
-
-  @property
-  def view_top_left(self):
-    return self._view_top_left
-
-  @view_top_left.setter
-  def view_top_left(self,value):
-    self._view_top_left = value
-    in_tiles = self._view_top_left_tiles()
-
-    # here the world active area is being potentially changed if the view rectangle is at the border of the active area:
-    if ((self.world.active_area[0] > 0 and in_tiles[0] <= self.world.active_area[0]) or
-        (self.world.active_area[1] > 0 and in_tiles[1] <= self.world.active_area[1]) or
-        (self.world.active_area[0] + self.world.active_area[2] < self.world.width and in_tiles[0] + WorldRenderer.VIEW_WIDTH_TILES >= self.world.active_area[0] + self.world.active_area[2]) or
-        (self.world.active_area[1] + self.world.active_area[3] < self.world.height and in_tiles[1] + WorldRenderer.VIEW_HEIGHT_TILES >= self.world.active_area[1] + self.world.active_area[3])):
-      print("changing active area")
-
-      self.__change_active_area()
-
-  def __init_attributes(self):
-    ## prerendered terrain of the active part of the world
-    self.terrain_image = None
-    ## position of the top left corner of the view rectangle in pixels
-    self._view_top_left = (0,0)
-    ## image to which the terrain will be rendered and which will be
-    #  returned as the rendered part of the world
-    self.canvas = pygame.Surface((WorldRenderer.VIEW_WIDTH,WorldRenderer.VIEW_HEIGHT))
-    ## world that is being rendered
-    self.world = None
-
-  ## Gets the pixel coordinates of the top left corner of the view
-  #  rectangle relative to the world active area.
-  #
-  #  @return pixel coordinates in format (x,y)
-
-  def view_top_left_relative(self):
-    return (self.view_top_left[0] - self.world.active_area[0] * general.TILE_WIDTH,self.view_top_left[1] - self.world.active_area[1] * general.TILE_HEIGHT)
-
-  def __init__(self,world):
-    self.__init_attributes()
-    self.world = world
-    self.__change_active_area()
-
-  ## Private method that is called to change the world active area and
-  #  prerender the terrain for it. The active area is set depending on
-  #  the current view coordinates.
-
-  def __change_active_area(self):
-    image_compositor = graphics.ImageCompositor()
-    view_tile_coordinates = self._view_top_left_tiles()
-
-    new_area = (general.saturate(view_tile_coordinates[0] - WorldRenderer.TILE_PADDING,0,self.world.width - WorldRenderer.ACTIVE_AREA_WIDTH),
-                general.saturate(view_tile_coordinates[1] - WorldRenderer.TILE_PADDING,0,self.world.height - WorldRenderer.ACTIVE_AREA_HEIGHT),
-                WorldRenderer.ACTIVE_AREA_WIDTH,
-                WorldRenderer.ACTIVE_AREA_HEIGHT)
-
-    self.world.active_area = new_area
-    self.terrain_image = image_compositor.make_terrain_image(self.world.world_area)
-
-  ## Renders the current world view.
-  #
-  #  @return the image (Surface object) of the rendered world area, its
-  #          size is defined by VIEW_WIDTH and VIEW_HEIGHT constants,
-  #          if the image couldn't be rendered (no world assigned etc.),
-  #          None is returned
-
-  def render(self):
-    self.canvas.fill((255,0,0,0))
-    view_relative = self.view_top_left_relative()
-    subsurface_rect = pygame.Rect(view_relative[0],view_relative[1],WorldRenderer.VIEW_WIDTH,WorldRenderer.VIEW_HEIGHT)
-    subsurface_rect = subsurface_rect.clip(self.terrain_image.get_rect())
-    self.canvas.blit(self.terrain_image,(-1 * view_relative[0],-1 * view_relative[1]))
-
-    return self.canvas
 
 #=======================================================================
 
 pygame.init()
-screen = pygame.display.set_mode((900,600))
+screen = pygame.display.set_mode((800,480))
 
 done = False
 
 w = world.World(general.RESOURCE_PATH + "/world")
-renderer = WorldRenderer(w)
+renderer = graphics.WorldRenderer(w)
 
 renderer.view_top_left = (20,50)
 
@@ -171,6 +59,6 @@ while not done:
 
   screen.fill((255,255,255))
 
-  screen.blit(renderer.render(),(10,10))
+  screen.blit(renderer.render(),(0,0))
 
   pygame.display.flip()
--- src/graphics.py
+++ src/graphics.py
@@ -8,21 +8,45 @@ import general
 import time
 import world
 import draw
+import math
 import random
+import time
+
+## Serves as a proxy image loader for tile images - all tile images
+#  access should be done via this class.
+#
+#  The class uses lazy image loading.
+
+class TileImageLoader:
+
+  ## A dictionary that holds the images, the keys are tile ids.
+
+  tile_images = {}
+
+  ## Gets an TileImageContainer of a tile with given tile_id.
+  #
+  #  @param tile_type TileType object to get the image for
+  #  @return tile image
+
+  def get_tile_image(tile_type):
+    if not (tile_type.identifier in TileImageLoader.tile_images):  # lazy image loading
+      TileImageLoader.tile_images[tile_type.identifier] = TileImageContainer(os.path.join(general.RESOURCE_PATH,"tile_" + tile_type.name + ".png"))
+
+    return TileImageLoader.tile_images[tile_type.identifier]
 
 #=======================================================================
 
 ## Holds images (i.e. main tile variations, corners etc.) of a game
-# (terrain) tile, the corners are represented by a string in following
-# format: cornar_AB_XY, where A is U or D (up, down), B is L or R
-# (left, right) and X and Y are either 0 (opposite not present) or 1
-# (opposite present).
-# For example if there is a part of terrain as follows:
-#   0 1
-# 0 K L
-# 1 K L
-# then for example in the upper left corner of L at [1,1] there should
-# be drawn a corner of K with identifier corner_UL_01.
+#  (terrain) tile, the corners are represented by a string in following
+#  format: cornar_AB_XY, where A is U or D (up, down), B is L or R
+#  (left, right) and X and Y are either 0 (opposite not present) or 1
+#  (opposite present).
+#  For example if there is a part of terrain as follows:
+#    0 1
+#    0 K L
+#    1 K L
+#  then for example in the upper left corner of L at [1,1] there should
+#  be drawn a corner of K with identifier corner_UL_01.
 
 class TileImageContainer:
   def init(self):
@@ -53,6 +77,7 @@ class TileImageContainer:
 
   def load_from_file(self,filename):
     image = pygame.image.load(filename)
+    image = image.convert_alpha()
 
     self.main_tile[0] = image.subsurface(68,0,general.TILE_WIDTH,general.TILE_HEIGHT)
     self.main_tile[1] = image.subsurface(102,0,general.TILE_WIDTH,general.TILE_HEIGHT)
@@ -81,8 +106,6 @@ class TileImageContainer:
 ## Assembles images out of image resources.
 
 class ImageCompositor:
-
-  #---------------------------------------------------------------------
 
   ## Makes a character image with given body, head, animation type, frame
   #  and gear
@@ -142,8 +165,6 @@ class ImageCompositor:
 
     return image1
 
-  #---------------------------------------------------------------------
-
   ## Helper private method that returns the priority of given tile type
   #  or 0 if the argument is not of TileType class.
 
@@ -153,8 +174,6 @@ class ImageCompositor:
     except Exception:
       return 0
 
-  #---------------------------------------------------------------------
-
   ## Helper private method, returns ordered list of all tile priorities
   #  that appear in given TerrainArray object.
 
@@ -163,14 +182,17 @@ class ImageCompositor:
 
     for j in range(terrain_array.height):
       for i in range(terrain_array.width):
-        priority = self.__tile_priority(terrain_array.get_tile_type(i,j))
+        tile_type = terrain_array.get_tile_type(i,j)
+
+        if tile_type != None and tile_type.animated:    # this maked animated tiles not render, they are drawn differently (the cannot be prerendered because they are changing constantly)
+          continue
+
+        priority = self.__tile_priority(tile_type)
 
         if not priority in result:
           result.append(priority)
 
     return sorted(result)
-
-  #---------------------------------------------------------------------
 
   ## Makes a terrain image.
   #
@@ -178,10 +200,8 @@ class ImageCompositor:
   #  @return Surface object - the generated image
 
   def make_terrain_image(self, terrain_array):
-    result_image = pygame.Surface((terrain_array.width * general.TILE_WIDTH, terrain_array.height * general.TILE_HEIGHT))
+    result_image = pygame.Surface((terrain_array.width * general.TILE_WIDTH, terrain_array.height * general.TILE_HEIGHT),flags = pygame.SRCALPHA)
     result_image.fill((255,255,255,0))
-
-    tile_pictures = {}
 
     for current_priority in self.__make_tile_priority_list(terrain_array):      # draw the terrain in layers
       for j in range(terrain_array.height):
@@ -199,11 +219,10 @@ class ImageCompositor:
           variant = terrain_array.get_tile_variant(i,j)
           tile_id = tile_type.identifier
 
-          if not (tile_id in tile_pictures):  # lazy image loading
-            tile_pictures[tile_id] = TileImageContainer(os.path.join(general.RESOURCE_PATH,"tile_" + tile_type.name + ".png"))
+          tile_picture = TileImageLoader.get_tile_image(tile_type)
 
           # draw the main tile:
-          result_image.blit(tile_pictures[tile_id].main_tile[variant],(i * general.TILE_WIDTH,j * general.TILE_HEIGHT))
+          result_image.blit(tile_picture.main_tile[variant],(i * general.TILE_WIDTH,j * general.TILE_HEIGHT))
 
           # draw corners and borders:
 
@@ -211,90 +230,227 @@ class ImageCompositor:
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
-           result_image.blit(tile_pictures[tile_id].corner_DR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+           result_image.blit(tile_picture.corner_DR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
 
           # UR corner
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
-            result_image.blit(tile_pictures[tile_id].corner_DL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
+            result_image.blit(tile_picture.corner_DL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
 
           # DR corner
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
-            result_image.blit(tile_pictures[tile_id].corner_UL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
+            result_image.blit(tile_picture.corner_UL_00,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
 
           # DL corner
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)) and \
              tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
-            result_image.blit(tile_pictures[tile_id].corner_UR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
+            result_image.blit(tile_picture.corner_UR_00,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
 
           # upper border
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j - 1)):
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)):  # left
-              helper_image = tile_pictures[tile_id].corner_DL_01
+              helper_image = tile_picture.corner_DL_01
             else:
-              helper_image = tile_pictures[tile_id].corner_DL_11
+              helper_image = tile_picture.corner_DL_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
 
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)):  # right
-              helper_image = tile_pictures[tile_id].corner_DR_01
+              helper_image = tile_picture.corner_DR_01
             else:
-              helper_image = tile_pictures[tile_id].corner_DR_11
+              helper_image = tile_picture.corner_DR_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH + general.SUBTILE_WIDTH,j * general.TILE_HEIGHT - general.SUBTILE_HEIGHT))
 
           # left border
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j)):
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j - 1)):  # up
-              helper_image = tile_pictures[tile_id].corner_UR_10
+              helper_image = tile_picture.corner_UR_10
             else:
-              helper_image = tile_pictures[tile_id].corner_UR_11
+              helper_image = tile_picture.corner_UR_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT))
 
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)):  # down
-              helper_image = tile_pictures[tile_id].corner_DR_10
+              helper_image = tile_picture.corner_DR_10
             else:
-              helper_image = tile_pictures[tile_id].corner_DR_11
+              helper_image = tile_picture.corner_DR_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH - general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.SUBTILE_HEIGHT))
 
           # right border
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j)):
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j - 1)):  # up
-              helper_image = tile_pictures[tile_id].corner_UL_10
+              helper_image = tile_picture.corner_UL_10
             else:
-              helper_image = tile_pictures[tile_id].corner_UL_11
+              helper_image = tile_picture.corner_UL_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT))
 
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)):  # down
-              helper_image = tile_pictures[tile_id].corner_DL_10
+              helper_image = tile_picture.corner_DL_10
             else:
-              helper_image = tile_pictures[tile_id].corner_DL_11
+              helper_image = tile_picture.corner_DL_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH + general.TILE_WIDTH,j * general.TILE_HEIGHT + general.SUBTILE_HEIGHT))
 
           # lower border
           if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i,j + 1)):
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i - 1,j + 1)):  # left
-              helper_image = tile_pictures[tile_id].corner_UL_01
+              helper_image = tile_picture.corner_UL_01
             else:
-              helper_image = tile_pictures[tile_id].corner_UL_11
+              helper_image = tile_picture.corner_UL_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
 
             if tile_priority > self.__tile_priority(terrain_array.get_tile_type(i + 1,j + 1)):  # right
-              helper_image = tile_pictures[tile_id].corner_UR_01
+              helper_image = tile_picture.corner_UR_01
             else:
-              helper_image = tile_pictures[tile_id].corner_UR_11
+              helper_image = tile_picture.corner_UR_11
 
             result_image.blit(helper_image,(i * general.TILE_WIDTH + general.SUBTILE_WIDTH,j * general.TILE_HEIGHT + general.TILE_HEIGHT))
 
     return result_image
 
-  #---------------------------------------------------------------------
+#=======================================================================
+
+## Efficiently renders a part of the game world with given settings.
+#
+#  The object of WorldRenderer class keeps a reference to World object
+#  which it will be rendering. It keeps a pre-rendered image of the
+#  world's active area terrain so it can be rendered quickly for
+#  real-time viewing. WorldRenderer will smartly change the world active
+#  area when the view shifts to the edge of the current area.
+
+class WorldRenderer:
+
+  ## view width in pixels
+  VIEW_WIDTH = 800
+
+  ## view height in pixels
+  VIEW_HEIGHT = 480
+
+  ## view width in pixels
+  VIEW_WIDTH_TILES = math.ceil(VIEW_WIDTH / general.TILE_WIDTH)
+
+  ## view height in pixels
+  VIEW_HEIGHT_TILES = math.ceil(VIEW_HEIGHT / general.TILE_HEIGHT)
+
+  ## along with VIEW_WIDTH and VIEW_HEIGHT determines the active area
+  #  size, which is width: VIEW_WIDTH_TILES + 2 * TILE_PADDING, height:
+  #  VIEW_HEIGHT_TILES + 2 * TILE_PADDING
+
+  TILE_PADDING = 15
+
+  ## active area width in tiles
+
+  ACTIVE_AREA_WIDTH = VIEW_WIDTH_TILES + 2 * TILE_PADDING
+
+  ## active area height in tiles
+
+  ACTIVE_AREA_HEIGHT = VIEW_HEIGHT_TILES + 2 * TILE_PADDING
+
+  def _view_top_left_tiles(self):
+    return (math.floor(self._view_top_left[0] / general.TILE_WIDTH),math.floor(self._view_top_left[1] / general.TILE_HEIGHT))
+
+  @property
+  def view_top_left(self):
+    return self._view_top_left
+
+  @view_top_left.setter
+  def view_top_left(self,value):
+    self._view_top_left = value
+    in_tiles = self._view_top_left_tiles()
+
+    # here the world active area is being potentially changed if the view rectangle is at the border of the active area:
+    if ((self.world.active_area[0] > 0 and in_tiles[0] <= self.world.active_area[0]) or
+        (self.world.active_area[1] > 0 and in_tiles[1] <= self.world.active_area[1]) or
+        (self.world.active_area[0] + self.world.active_area[2] < self.world.width and in_tiles[0] + WorldRenderer.VIEW_WIDTH_TILES >= self.world.active_area[0] + self.world.active_area[2]) or
+        (self.world.active_area[1] + self.world.active_area[3] < self.world.height and in_tiles[1] + WorldRenderer.VIEW_HEIGHT_TILES >= self.world.active_area[1] + self.world.active_area[3])):
+      print("changing active area")
+
+      self.__change_active_area()
+
+  def __init_attributes(self):
+    ## prerendered terrain of the active part of the world
+    self.terrain_image = None
+    ## position of the top left corner of the view rectangle in pixels
+    self._view_top_left = (0,0)
+    ## image to which the terrain will be rendered and which will be
+    #  returned as the rendered part of the world
+    self.canvas = pygame.Surface((WorldRenderer.VIEW_WIDTH,WorldRenderer.VIEW_HEIGHT))
+    ## world that is being rendered
+    self.world = None
+
+  ## Gets the pixel coordinates of the top left corner of the view
+  #  rectangle relative to the world active area.
+  #
+  #  @return pixel coordinates in format (x,y)
+
+  def view_top_left_relative(self):
+    return (self.view_top_left[0] - self.world.active_area[0] * general.TILE_WIDTH,self.view_top_left[1] - self.world.active_area[1] * general.TILE_HEIGHT)
+
+  ## Same as view_top_left_relative, just returns tiles plus pixel
+  #  difference.
+  #
+  #  @return tuple in format (x,y,pixel_diff_x,pixel_diff_y),
+  #          pixel_diff_x and pixel_diff_y are the pixel difference
+  #          relative to given tile
+
+  def view_top_left_relative_tiles(self):
+    pixel_coordinates = self.view_top_left_relative()
+    x = math.floor(pixel_coordinates[0] / general.TILE_WIDTH)
+    y = math.floor(pixel_coordinates[1] / general.TILE_HEIGHT)
+    return (x,y,pixel_coordinates[0] - x * general.TILE_WIDTH,pixel_coordinates[1] - y * general.TILE_HEIGHT)
+
+  def __init__(self,world):
+    self.__init_attributes()
+    self.world = world
+    self.__change_active_area()
+
+  ## Private method that is called to change the world active area and
+  #  prerender the terrain for it. The active area is set depending on
+  #  the current view coordinates.
+
+  def __change_active_area(self):
+    image_compositor = ImageCompositor()
+    view_tile_coordinates = self._view_top_left_tiles()
+
+    new_area = (general.saturate(view_tile_coordinates[0] - WorldRenderer.TILE_PADDING,0,self.world.width - WorldRenderer.ACTIVE_AREA_WIDTH),
+                general.saturate(view_tile_coordinates[1] - WorldRenderer.TILE_PADDING,0,self.world.height - WorldRenderer.ACTIVE_AREA_HEIGHT),
+                WorldRenderer.ACTIVE_AREA_WIDTH,
+                WorldRenderer.ACTIVE_AREA_HEIGHT)
+
+    self.world.active_area = new_area
+    self.terrain_image = image_compositor.make_terrain_image(self.world.world_area)
+
+  ## Renders the current world view.
+  #
+  #  @return the image (Surface object) of the rendered world area, its
+  #          size is defined by VIEW_WIDTH and VIEW_HEIGHT constants,
+  #          if the image couldn't be rendered (no world assigned etc.),
+  #          None is returned
+
+  def render(self):
+    self.canvas.fill((255,0,0,0))
+
+    view_top_left_tile = self.view_top_left_relative_tiles()
+
+    # prepass, get objects in view to be rendered plus render animated files
+    y = -view_top_left_tile[3]
+    for j in range(view_top_left_tile[1],view_top_left_tile[1] + WorldRenderer.ACTIVE_AREA_HEIGHT):
+      x = -view_top_left_tile[2]
+      for i in range(view_top_left_tile[0],view_top_left_tile[0] + WorldRenderer.ACTIVE_AREA_WIDTH):
+        tile_type = self.world.world_area.get_tile_type(i,j)
+        if tile_type.animated:
+          self.canvas.blit(TileImageLoader.get_tile_image(tile_type).main_tile[int(time.time()) % 4],(x,y))
+        x += general.TILE_WIDTH
+      y += general.TILE_HEIGHT
+
+    view_relative = self.view_top_left_relative()
+    self.canvas.blit(self.terrain_image,(-1 * view_relative[0],-1 * view_relative[1]))
+    return self.canvas
